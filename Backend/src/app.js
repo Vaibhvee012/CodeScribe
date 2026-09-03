008a3ecaeced8577fcca4ddf43922d66373a8733
Vaibhvee Prakash <vaibhveeprakash25@gmai.com>
2026-09-03 16:59:54 +0530

    fix CORS and deployment configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,11 +7,22 @@ const app =  express();
 
 
 //middlewares
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://code-scribe-ashen.vercel.app",
+    "https://code-scribe-g58z97704-vibe-7cbc.vercel.app"
+];
+
 app.use(cors({
-   origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
 }));
-
 app.use(express.json());
 
 app.use("/api", codeReviewRoutes);
